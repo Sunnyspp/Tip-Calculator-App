b0aab990ad3a41a551fa4034e924de141c9cfad4
Sunnyspp <sunnyspp2013@gmail.com>
2023-01-25 01:49:53 -0800

    updated

---
+++ README.md
@@ -0,0 +1,8 @@
+TIP CALCULATOR:
+    
+The aim of this program is to help you practise the following concept in python:
+How to declare variables the right way.
+How to use the print() and input()
+How to convert strings to float or int by using casting.
+How to use print(f";;;;;;;;;;;")
+How to use the emoji
---
+++ tip_calculator.py
@@ -0,0 +1,14 @@
+# Tip Calculator App
+food_amount = float(input('Enter food amount $: '))
+tip_percentage = float(input('Enter your tip percentage %: ')) / 100
+tip_amount = food_amount * tip_percentage
+
+# total food_amount + tip_amount
+total = food_amount + tip_amount
+print('\n\n\n')
+print('----------------------------------')
+print(f'🍔Food Amount: ${food_amount}')
+print(f'💰Tip Amount: ${tip_amount}')
+print('\n')
+print(f'💰Total Amount: ${total}')
+print('------------------------------------')
